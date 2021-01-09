1a2a030f6d70387b969f9e77e119e7904da7c69d
qw-null <qinweirz@qq.com>
2021-01-09 23:43:22 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Hello world!!
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'Home'
+}
+</script>
+
+<style type="text/css">
+
+</style>
