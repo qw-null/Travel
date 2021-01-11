b5548b71b143fd926e082af58b63366e1ab5589d
qw-null <qinweirz@qq.com>
2021-01-11 14:25:39 +0800

    add recommend

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom"
+          v-for="item of recommendList"
+          :key="item.id"
+      >
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{ item.title }}</p>
+          <p class="item-desc">{{ item.desc }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '001',
+        imgUrl: 'https://imgs.qunarzz.com/vs_ceph_vcimg/f03f5ac90ae59d0d9c6332a2bfd9782e.jpeg',
+        title: '故宫',
+        desc: '世界五大宫之首，穿越与您近在咫尺'
+      }, {
+        id: '002',
+        imgUrl: 'https://imgs.qunarzz.com/vs_ceph_vcimg/f03f5ac90ae59d0d9c6332a2bfd9782e.jpeg',
+        title: '故宫',
+        desc: '世界五大宫之首，穿越与您近在咫尺'
+      }, {
+        id: '003',
+        imgUrl: 'https://imgs.qunarzz.com/vs_ceph_vcimg/f03f5ac90ae59d0d9c6332a2bfd9782e.jpeg',
+        title: '故宫',
+        desc: '世界五大宫之首，穿越与您近在咫尺'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "~styles/mixins.styl"
+.title{
+  margin-top: .2rem;
+  line-height: .8rem;
+  background: #eee;
+  text-indent: .2rem;
+}
+.item-img-wrapper{
+  overflow: hidden;
+  height: 0;
+  padding-bottom: 33.9%;
+
+}
+.item-img{
+  width: 100%;
+
+}
+.item-info{
+  flex: 1;
+  padding: .1rem;
+  min-width: 0;
+}
+.item-title{
+  line-height: .54rem;
+  font-size: .32rem;
+  ellipsis()
+}
+.item-desc{
+  line-height: .4rem;
+  color: #ccc;
+  ellipsis()
+}
+</style>
