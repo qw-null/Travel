e13d08e38aca83283934b964efda31a777d75118
qw-null <qinweirz@qq.com>
2021-01-11 23:10:31 +0800

    ajax city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,24 +2,48 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :cities="cities" :hotCities="hotCities"></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
 import CityAlphabet from './components/Alphabet'
 export default {
   name: 'City',
+  methods: {
+    getCityInfo: function () {
+      axios.get('/api/city.json').then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+
+      console.log(res)
+    }
+  },
+  mounted () {
+    this.getCityInfo()
+  },
   components: {
     CityHeader,
     CitySearch,
     CityList,
     CityAlphabet
-
+  },
+  data: function () {
+    return {
+      cities: {},
+      hotCities: []
+    }
   }
 
 }
