7c7cd48731d6df48c12ab0acc520f8e3230c0718
qw-null <qinweirz@qq.com>
2021-01-12 21:10:24 +0800

    Finish Pages

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import {mapState} from 'vuex'
 export default {
   name: 'Home',
   data: function () {
@@ -22,7 +23,8 @@ export default {
       swiperList: [],
       iconList: [],
       weekendList: [],
-      recommendList: []
+      recommendList: [],
+      lastCity: ''
     }
   },
   components: {
@@ -33,11 +35,24 @@ export default {
     HomeWeekend
   },
   mounted () {
+    // console.log('mounted')
     this.getHomeInfo()
+    this.lastCity = this.city
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
+    // console.log('activated')
+  },
+  computed: {
+    ...mapState(['city'])
+
   },
   methods: {
     getHomeInfo: function () {
-      axios.get('/api/index.json').then(this.getHomeInfoSucc)
+      axios.get('/api/index.json?city=' + this.city).then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc: function (res) {
       res = res.data
